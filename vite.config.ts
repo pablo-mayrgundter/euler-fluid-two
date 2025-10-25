c52b708ea88c9ad37d63c48333459ece2c7781c5
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2025-10-25 16:47:42 +0200

    Add base and build configuration to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE ?? '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
